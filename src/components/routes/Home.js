4c5c3a885cc5bb835b2ef71e406b1fedebfc8cf1
Mister Singh <mistersingh179@gmail.com>
2022-12-07 18:21:23 -0500

    added intercom

--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -1,11 +1,22 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import { Container } from "@chakra-ui/react";
 import TopNav from "../setup/TopNav";
 import BrowserIssue from "../setup/BrowserIssue";
-
-
+import { useEffect } from "react";
 
 export default function Home(props) {
+  useEffect(() => {
+    window.Intercom("boot", {
+      api_base: "https://api-iam.intercom.io",
+      app_id: "vesl7md6",
+    });
+  }, []);
+
+  const location = useLocation();
+  useEffect(() => {
+    window.Intercom("update");
+  }, [location]);
+
   return (
     <>
       <TopNav />
